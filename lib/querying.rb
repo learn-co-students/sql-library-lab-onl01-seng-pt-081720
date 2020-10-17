2689271eb0e6b1bdf02d449f545838377dc224cf
jasonemarkson <jasonmarkson76@gmail.com>
2020-10-17 15:54:43 -0400

    Done.

--- lib/querying.rb
+++ lib/querying.rb
@@ -16,7 +16,7 @@ end
 def select_name_and_series_subgenres_of_authors
   "SELECT authors.name, subgenres.name FROM authors
   JOIN subgenres
-  GROUP BY subgenres.name"
+  GROUP BY DISTINCT subgenres.name"
 end
 
 def select_series_title_with_most_human_characters
