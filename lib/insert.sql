dc4372ff5e5236c36febf7f27f2345fe24cbd5f6
Christian Pardo <chrispardopersonal@gmail.com>
2020-10-25 09:38:33 +0000

    Done.

--- lib/insert.sql
+++ lib/insert.sql
@@ -4,6 +4,6 @@ INSERT INTO characters (id, name, species, motto, author_id) VALUES (1, "Jon Sno
 INSERT INTO characters (id, name, species, motto, author_id) VALUES (5, "Galadriel", "elf", "Even the smallest person can change the course of the future.", 2), (6, "Arwen", "elf", "I would rather share one lifetime with you than face all the Ages of this world alone.", 2), (7, "Aragorn", "human", "A day may come when the courage of men fails… but it is not THIS day.", 2), (8, "Gandalf", "human", "The board is set, the pieces are moving. We come to it at last, the great battle of our time.", 2);
 INSERT INTO subgenres (id, name) VALUES (1, "fantasy"), (2, "high fantasy");
 INSERT INTO authors (id, name) VALUES (1, "George R. R. Martin"), (2, "J. R. R. Tolkien");
-INSERT INTO characters_books (id, book_id, character_id) VALUES (1, 1, 3), (2, 2, 1), (3, 3, 4), (4, 1, 2), (5, 2, 3), (6, 3, 1), (7, 1, 4), (8, 3, 2);
-INSERT INTO characters_books (id, book_id, character_id) VALUES (9, 1, 3), (10, 2, 1), (11, 3, 4), (12, 1, 2), (13, 2, 3), (14, 3, 1), (15, 1, 4), (16, 3, 2);
+INSERT INTO character_books (id, book_id, character_id) VALUES (1, 1, 3), (2, 2, 1), (3, 3, 4), (4, 1, 2), (5, 2, 3), (6, 3, 1), (7, 1, 4), (8, 3, 2);
+INSERT INTO character_books (id, book_id, character_id) VALUES (9, 4, 5), (10, 5, 8), (11, 6, 6), (12, 4, 7), (13, 5, 8), (14, 6, 6), (15, 6, 5), (16, 5, 7);
 
