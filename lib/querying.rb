1b80647fc2ab5329da161a8c0aa10873bae303ca
Janaeq <janaemassa@gmail.com>
2020-11-22 02:21:36 -0500

    Done.

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,49 +1,55 @@
 def select_books_titles_and_years_in_first_series_order_by_year
-  "SELECT books.title, books.year 
+  "SELECT books.title, books.year
   FROM books
-  WHERE series_id = 1"
+  WHERE books.series_id = 1
+  ORDER BY books.year"
 end
 
 def select_name_and_motto_of_char_with_longest_motto
   "SELECT characters.name, characters.motto
   FROM characters
-  ORDER BY LENGTH(characters.motto) DESC
+  ORDER BY LENGTH(motto) DESC
   LIMIT 1"
 end
 
 
 def select_value_and_count_of_most_prolific_species
-  "SELECT species, COUNT (*) AS 'value_occurrence'
+  "SELECT characters.species, COUNT(*)
   FROM characters
-  GROUP BY species
-  ORDER BY 'value_occurrence' DESC
-  LIMIT 1"
+  GROUP BY characters.species
+  ORDER BY COUNT(characters.species) DESC
+  LIMIT 1;"
 end
 
 def select_name_and_series_subgenres_of_authors
   "SELECT authors.name, subgenres.name
   FROM authors
-  INNER JOIN series
-  ON series.author_id = authors.id
-  INNER JOIN subgenres
-  ON series.subgenre_id = subgenres.id;"
+  JOIN series
+  ON authors.id = series.author_id
+  JOIN subgenres
+  ON subgenres.id = series.subgenre_id"
 end
 
 def select_series_title_with_most_human_characters
   "SELECT series.title
   FROM series
-  INNER JOIN characters
-  ON series.author_id = characters.author_id
-  GROUP BY species
-  ORDER BY COUNT(characters.species) DESC
-  LIMIT 1;"
+  JOIN books
+  ON series.id = books.series_id
+  JOIN character_books
+  ON character_books.book_id = books.id
+  JOIN characters
+  ON character_books.character_id = characters.id
+  WHERE characters.species = 'human'    
+  GROUP BY series.title
+  ORDER BY COUNT(*) DESC
+  LIMIT 1"
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "SELECT characters.name, COUNT(character_books.book_id) AS book_value
-  FROM characters
-  INNER JOIN character_books
-  ON characters.id = character_books.character_id
-  GROUP BY characters.name
-  ORDER BY book_value DESC, characters.name"
+  "SELECT characters.name, COUNT(*) as book_count 
+  FROM character_books 
+  JOIN characters 
+  ON character_books.character_id = characters.id 
+  GROUP BY characters.name 
+  ORDER BY book_count DESC, characters.name ASC"
 end
